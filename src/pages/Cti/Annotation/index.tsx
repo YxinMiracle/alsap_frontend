1c92276ed5e8e31761a55d473c485b296e9c2495
YxinMiracle <939157765@qq.com>
2024-09-03 23:11:47 +0800

    模型抽取结果展示界面

--- src/pages/Cti/Annotation/index.tsx
+++ src/pages/Cti/Annotation/index.tsx
@@ -1,14 +1,19 @@
 import EntityItemBox from '@/components/AnnotationComponent/EntityItemBox';
-import { getDetailCtiUsingPost } from '@/services/backend/ctiController';
+import accessEnum from '@/constants/access/accessEnum';
+import '@/pages/Cti/Annotation/style/annotationStyle.css';
+import {
+  getDetailCtiUsingPost,
+  getPageCtiWordLabelListUsingPost,
+} from '@/services/backend/ctiController';
 import { getAllItemMapDataUsingGet } from '@/services/backend/itemController';
-import {useModel, useParams} from '@@/exports';
+import { useModel, useParams } from '@@/exports';
 import { PageContainer } from '@ant-design/pro-components';
 import '@umijs/max';
-import { Card, Col, FloatButton, message, Row } from 'antd';
+import { Button, Card, Col, Drawer, FloatButton, message, Row, Tag, Typography } from 'antd';
 import ReactECharts, { EChartsOption } from 'echarts-for-react';
 import React, { useEffect, useState } from 'react';
-import echarts from 'echarts';
-import echartsDarkTheme from '@/constants/dackChatTheme'
+
+const { Text, Title } = Typography;
 
 /**
  * 用户管理页面
@@ -22,8 +27,37 @@ const CtiAnnotationPage: React.FC = () => {
   const [ctiContent, setCtiContent] = useState<string>();
   const [itemData, setItemData] = useState<API.Item[]>([]);
   const { initialState } = useModel('@@initialState');
-  const [sysTheme, setSysTheme] = useState<string>()
+  const { currentUser } = initialState || {};
+  const [sysTheme, setSysTheme] = useState<string>();
+  const [open, setOpen] = useState(false);
+  const [requestWordLabelListParams, setRequestWordLabelListParams] =
+    useState<API.CtiModelWordLabelResultDto>({
+      ctiId: id,
+      size: 10,
+      current: 1,
+    });
+  const [wordLabelResult, setWordLabelList] = useState<API.CtiWordLabelVo>({
+    wordList: [],
+    labelList: [],
+  });
 
+  /**
+   * 开启模型抽取结果的抽屉
+   */
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  /**
+   * 开启模型抽取结果的抽屉关闭逻辑
+   */
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  /**
+   * 初始化界面获取数据
+   */
   const loadData = async () => {
     if (!id) {
       message.error('请勿非法访问该界面');
@@ -125,9 +159,8 @@ const CtiAnnotationPage: React.FC = () => {
               shadowColor: 'rgba(0, 0, 0, 0.5)',
             },
           },
-          bottom: "12%"
+          bottom: '12%',
         },
-
       ],
     };
     setChartOption(option);
@@ -152,15 +185,42 @@ const CtiAnnotationPage: React.FC = () => {
     setCtiChunkList(ctiChunkList);
   };
 
+  /**
+   * 获取情报中的标签和单词数据，模型训练好的
+   */
+  const getWordLabelData = async () => {
+    try {
+      const res = await getPageCtiWordLabelListUsingPost(requestWordLabelListParams);
+      if (res.code === 0) {
+        setWordLabelList((prevState) => ({
+          // @ts-ignore
+          wordList: [...prevState.wordList, ...res.data.wordList],
+          labelList: [...prevState.labelList, ...res.data.labelList],
+        }));
+        setRequestWordLabelListParams((prevState) => ({
+          ...prevState,
+          // @ts-ignore
+          current: prevState.current + 1,
+        }));
+      }
+    } catch (e: any) {
+      message.error('获取数据出错', e);
+    }
+  };
+
   useEffect(() => {
+    // 获取cti情报的详情数据
     loadData();
+    // 获取item的全部数据
     loadItemData();
+    // 获取模型标注数据
+    getWordLabelData();
   }, []);
 
   useEffect(() => {
     // @ts-ignore
     const navTheme = initialState.settings.navTheme;
-    setSysTheme(navTheme==='light'?'':'dark')
+    setSysTheme(navTheme === 'light' ? '' : 'dark');
   }, [initialState.settings.navTheme]);
 
   useEffect(() => {
@@ -193,8 +253,98 @@ const CtiAnnotationPage: React.FC = () => {
             <Col sm={{ span: 24 }} lg={{ span: 12 }}>
               {/*<Col span={12}>*/}
               <div style={{ position: 'sticky', top: 56 }}>
-                <Card title="威胁情报信息展示区域" bordered={false} hoverable>
+                <Card
+                  title="威胁情报信息展示区域"
+                  className="annotation-right-card"
+                  bordered={false}
+                  hoverable
+                >
                   <ReactECharts theme={sysTheme} option={chartOption} style={{ minHeight: 360 }} />
+                </Card>
+                <Card
+                  title="操作功能区域"
+                  className="annotation-right-card"
+                  bordered={false}
+                  hoverable
+                >
+                  {currentUser?.userRole === accessEnum.ADMIN && (
+                    <Button className="annotation-right-card-btn" size="large">
+                      提交标注内容
+                    </Button>
+                  )}
+                  <Button
+                    size="large"
+                    onClick={() => showDrawer()}
+                    className="annotation-right-card-btn"
+                  >
+                    查看模型识别结果
+                  </Button>
+                  <Drawer
+                    width={1200}
+                    style={{ backgroundColor: '#efefef' }}
+                    title="Basic Drawer"
+                    onClose={onClose}
+                    open={open}
+                  >
+                    <div>
+                      {wordLabelResult.wordList.map((word_list: string[], list_index: number) => (
+                        <>
+                          <Title level={3}>第 {list_index+1} 句: </Title>
+                          <Card bordered={false} className="annotation-right-card">
+                            <Row gutter={16} key={list_index}>
+                              {' '}
+                              {/* 注意增加 key 用于优化渲染性能 */}
+                              {word_list.map((word: string, word_index: number) => (
+                                <Col span={4} key={word_index}>
+                                  {' '}
+                                  {/* 同样增加 key */}
+                                  <div className="annotation-sent-card single-word-div">
+                                    <div
+                                      className={
+                                        wordLabelResult.labelList[list_index][word_index] === 'O'
+                                          ? 'normal-text'
+                                          : ''
+                                      }
+                                    >
+                                      {wordLabelResult.labelList[list_index][word_index] === 'O' ? (
+                                        <Text>{word}</Text>
+                                      ) : (
+                                        <Title level={4}>{word}</Title>
+                                      )}
+                                    </div>
+                                    <div>
+                                      <Tag
+                                        color={
+                                          wordLabelResult.labelList[list_index][word_index] === 'O'
+                                            ? 'default'
+                                            : 'processing'
+                                        }
+                                      >
+                                        {wordLabelResult.labelList[list_index][word_index]}
+                                      </Tag>{' '}
+                                      {/* 单词作为Tag显示 */}
+                                    </div>
+                                  </div>
+                                </Col>
+                              ))}
+                            </Row>
+                          </Card>
+                        </>
+
+                      ))}
+                    </div>
+                  </Drawer>
+                  <Button className="annotation-right-card-btn" size="large">
+                    查看情报图谱
+                  </Button>
+                  {currentUser?.userRole === accessEnum.ADMIN && (
+                    <Button className="annotation-right-card-btn" size="large">
+                      复制模型识别结果
+                    </Button>
+                  )}
+                  <Button className="annotation-right-card-btn" size="large">
+                    大模型关系抽取
+                  </Button>
                 </Card>
               </div>
             </Col>
